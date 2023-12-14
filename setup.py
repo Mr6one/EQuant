4eafd752b6fe8725ececfeb524ebbfc23ec0de60
Mr6one <kos59.s@yandex.ru>
2023-12-14 11:58:01 +0300

    package distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='equant',
+    version='0.0.1',
+    description='A collection of tools for neural networks quantization',
+    author='Konstantin Shlychkov',
+    author_email='shlychkov.kd@phystech.edu',
+    packages=find_packages(),
+)
